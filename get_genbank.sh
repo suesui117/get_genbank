c94a4534a03d83c87dc422a46e874e881ac03e48
suesui117 <sui.xi@husky.neu.edu>
2020-06-22 23:54:20 -0400

    get and parse GenBank file

--- get_genbank.sh
+++ get_genbank.sh
@@ -1,33 +1,29 @@
 #!/usr/bin/env bash
 
 
-if [ ! -d "$(eval "pwd")/edirect" ]; then
+if [ ! -d "$(echo ~)/edirect" ]; then
     sh -c "$(curl -fsSL ftp://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/install-edirect.sh)"
 fi
 
 
-echo Please enter a preferred transcript from GRCh37: e.g. NM_020742:
+echo Please enter a GRCh37 preferred transcript without version number: e.g. NM_020742:
 
-read varname
+read input
+
+varname=$(echo "$input" | sed -E -e 's/\.[0-9]+.*|\..*//g')
+
+if ! [[  $varname = NM_[0-9]* ||  $varname = NR_[0-9]*  ]]; then
+  echo "Wrong accession format"
+  exit
+fi
+
+echo $varname
 
 mkdir -p auto_genbank
 
-gene=$(esearch -db nuccore -query "$varname" | elink -target gene |efetch -format name|sed -n -e 's/^1.* //p')
+gene=$(esearch -db nuccore -query "$varname" | elink -target gene | efetch -format name|sed -n -e 's/^1.* //p')
 echo $gene
 
 esearch -db gene -query "$gene"'[gene] AND human[orgn]' |  efetch -format docsum |  xtract -pattern DocumentSummary -block LocationHistType -if AnnotationRelease -equals 105 -tab "\n" -element ChrAccVer,ChrStart,ChrStop | grep NC_0000 | awk -F '\t' '{{OFS = "\t"} if ($2 < $3) {print $1, $2+1, $3+1} else {print $1, $2+1, $3+1}}' | xargs -n 3 sh -c 'efetch -db nucleotide -id "$0" -seq_start "$1" -seq_stop "$2" -format gbwithparts' > auto_genbank/"$gene"_"$varname"_.gbk
 
 python3 parse_preferred_transcript.py
-
-# file=/Users/sue/Desktop/BINF6200/Andy/genes
-# while IFS='' read -r line || [ "$line" ]
-# do
-
-#     esearch -db gene -query "$line"'[gene] AND human[orgn]' |  efetch -format docsum |  xtract -pattern DocumentSummary -block LocationHistType -if AnnotationRelease -equals 105 -tab "\n" -element ChrAccVer,ChrStart,ChrStop | grep NC_0000 | awk -F '\t' '{{OFS = "\t"} if ($2 < $3) {print $1, $2+1, $3+1} else {print $1, $2+1, $3+1}}' | xargs -n 3 sh -c 'efetch -db nucleotide -id "$0" -seq_start "$1" -seq_stop "$2" -format gbwithparts' > ok.gbk
-#     printf '%s\n' "$line"
-
-# done < "$file"
-
-# mkdir -p auto_genbank
-
-# esearch -db gene -query "$varname"'[gene] AND human[orgn]' |  efetch -format docsum |  xtract -pattern DocumentSummary -block LocationHistType -if AnnotationRelease -equals 105 -tab "\n" -element ChrAccVer,ChrStart,ChrStop | grep NC_0000 | awk -F '\t' '{{OFS = "\t"} if ($2 < $3) {print $1, $2+1, $3+1} else {print $1, $2+1, $3+1}}' | xargs -n 3 sh -c 'efetch -db nucleotide -id "$0" -seq_start "$1" -seq_stop "$2" -format gbwithparts' > auto_genbank/"$varname"_.gbk
--- parse_preferred_transcript.py
+++ parse_preferred_transcript.py
@@ -1,80 +1,96 @@
 # !/usr/bin/env python3
 
 """
-The version of transcript accession does NOT matter because we are using GRCh37 build,
-so it will be older versions. In addition, we should NOT use e.g. NM_025114.3 as user input
-because Entrez cannot find it sometimes.
+Parses GenBank file and outputs preferred transcript GenBank file
 """
 
 import os
 import re
 import sys
+import glob
 from Bio import GenBank, Entrez
 
-Entrez.email = "suesui117@gmail.com"
-directory = os.getcwd()+"/auto_genbank/"
-path = ""
-gene = ""
-user_nm = ""
-gb_nm = ""
-np = ""
-nc = ""
 
-for i in os.listdir(directory):
-    if i.endswith(".gbk"):
-        path = directory + i
-        gene = i.split("_")[0]
-        user_nm = "_".join([i.split("_")[1], i.split("_")[2]])
+def parse_path(in_path, user_nm):
+    with open(in_path) as fh:
+        for record in GenBank.parse(fh):
+            try:
+                rec = str(record)
+                if user_nm not in rec:
+                    nm = user_nm.split(".")[0]
+                    print("Hello, user_nm not in record")
+                    gb_nm = re.search(r"(N[M]_\d+)", Entrez.efetch(db="nuccore", id=nm, rettype="acc", retmode="text").read()).group(0)
+                else:
+                    gb_nm = re.search(r"(N[M]_\d+)", Entrez.efetch(db="nuccore", id=user_nm, rettype="acc", retmode="text").read()).group(
+                        0)
 
-        if os.path.getsize(path) != 0:  # exclude empty size files
-            with open(path) as fh:
-                for record in GenBank.parse(fh):
+                np = re.search(r"(NP_\d+.\d)", Entrez.efetch(db="nuccore", id=gb_nm, rettype="gb", retmode="text").read()).group(1).split(".")[
+                        0]
+                nc = re.search(r"(VERSION\s+)(NC_\d+.\d+)", rec).group(2)
+
+            except AttributeError:  # if attributeError, it will be NR RNA accessions, we don't parse, leave as it is
+                print("AttributeError" + in_path)
+                if "NR_" in user_nm and "NR_" in rec:
+                    gb_nm = re.search(r"(N[R]_\d+)", Entrez.efetch(db="nuccore", id=user_nm, rettype="acc", retmode="text").read()).group(
+                        0)
+                    nc = re.search(r"(VERSION\s+)(NC_\d+.\d+)", rec).group(2)
+    return gb_nm, np, nc
+
+
+def write_outfile(path, gene, unpack):
+    with open(path) as fh:
+        lst = []
+        (gb_nm, np, nc) = unpack
+
+        final_nm = ""
+        for record in GenBank.parse(fh):
+            for every_chunk in record.features:
+                if (gb_nm in str(every_chunk) and gene in str(every_chunk)) or (gene in str(every_chunk) and np.split(".")[0] in str(
+                        every_chunk)):
+                    lst.append(str(every_chunk))
+
                     try:
-                        rec = str(record)
-                        if user_nm not in rec:
-                            nm = user_nm.split(".")[0]
-                            print("Hello, user_nm not in record")
-                            gb_nm = re.search(r"(N[M]_\d+)", Entrez.efetch(db="nuccore", id=nm, rettype="acc", retmode="text").read()).group(0)
+                        final_nm = re.search(r"(N[M|R]_\d+.\d+)", str(every_chunk)).group(0)
+                        print(final_nm)
+                    except AttributeError:
+                        pass
 
-                        else:
-                            gb_nm = re.search(r"(N[M]_\d+)", Entrez.efetch(db="nuccore", id=user_nm, rettype="acc", retmode="text").read()).group(0)
+            total = "".join(lst)
+            if len(lst) == 0 or len(lst) > 2:
+                sys.exit()
 
-                        np = re.search(r"(NP_\d+.\d)", Entrez.efetch(db="nuccore", id=gb_nm, rettype="gb", retmode="text").read()).group(1).split(".")[0]
-                        nc = re.search(r"(VERSION\s+)(NC_\d+.\d+)", rec).group(2)
+            field1_field2 = "".join([str(record.features[0]), str(record.features[1])])
+            record.features = field1_field2 + total
+            return record, final_nm, nc
 
-                    except AttributeError:  # if attributeError, it will be NR RNA accessions, we don't parse, leave as it is
-                        print("AttributeError" + path)
-                        if "NR_" in user_nm and "NR_" in rec:
-                            gb_nm = re.search(r"(N[R]_\d+)", Entrez.efetch(db="nuccore", id=user_nm, rettype="acc", retmode="text").read()).group(0)
-                            nc = re.search(r"(VERSION\s+)(NC_\d+.\d+)", rec).group(2)
-                            print("This is an NR_", gb_nm)
-            print(gb_nm, np, nc, gene)
-            with open(path) as fh:
-                ok = None
-                rna = None
-                lst = []
 
-                final_nm = ""
-                for record in GenBank.parse(fh):
-                    for every_chunk in record.features:
-                        if (gb_nm in str(every_chunk) and gene in str(every_chunk)) or (gene in str(every_chunk) and np.split(".")[0] in str(
-                                every_chunk)):
-                            lst.append(str(every_chunk))
+def main():
+    """The main function of the script."""
 
-                            try:
-                                final_nm = re.search(r"(N[M|R]_\d+.\d+)", str(every_chunk)).group(0)
-                                print(final_nm)
-                            except AttributeError:
-                                pass
+    Entrez.email = "suesui117@gmail.com"
+    directory = os.getcwd() + "/auto_genbank/"
 
-                    total = "".join(lst)
-                    print(len(lst))
-                    if len(lst) == 0 or len(lst) > 2:
-                        sys.exit()
+    while len(os.listdir(directory)) > 1:
+        files_path = os.path.join(directory, '*')
+        files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True)
+        for f in files[1:]:
+            os.unlink(f)
 
-                    field1_field2 = "".join([str(record.features[0]), str(record.features[1])])
-                    record.features = field1_field2 + total
-                    outfile = "_".join([gene, final_nm, nc]) + ".gbk"
-                    with open(outfile, "w") as file:
-                        file.write(str(record))
-                        os.remove(path)
+    for i in os.listdir(directory):
+        if i.endswith(".gbk"):
+            path = directory + i
+            gene = i.split("_")[0]
+            user_nm = "_".join([i.split("_")[1], i.split("_")[2]])
+
+            unpack = parse_path(path, user_nm)
+            (record, final_nm, nc) = write_outfile(path, gene, unpack)  # This function returns record, final_nm and nc for output writing
+
+            outfile = "_".join([gene, final_nm, nc]) + ".gbk"
+            with open(outfile, "w") as file:
+                file.write(str(record))
+                os.remove(path)
+
+
+if __name__ == "__main__":
+    main()
+
